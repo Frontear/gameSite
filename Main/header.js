66f0b98fbfadc118ae3c3faad63c641f10363957
Ali Rizvi <alirizvi609@gmail.com>
2017-12-20 07:35:47 +0900

    Put header externally
    This allows us to modify one file and update everything.

---
+++ Main/header.js
@@ -0,0 +1,3 @@
+﻿window.onload = function () {
+	document.getElementById("header").innerHTML = '<ul><li> <a href="index.html"><i id="icon" class="fa fa-home" aria-hidden="true"></i>&nbsp;Home</a></li> <li><a href="hrt.html"><i id="icon" class="fa fa-user" aria-hidden="true"></i>&nbsp;Human Reaction</a></li> <li><a href="ctc.html"><i id="icon" class="fa fa-circle-o" aria-hidden="true"></i>&nbsp;Catch Circles</a></li> <li style="float: right;"><a id="time"></a></li> </ul>';
+}
