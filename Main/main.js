9e3b284d4a34efaef182f45ea4f76a182baff33c
Ali Rizvi <alirizvi609@gmail.com>
2018-01-10 10:20:42 +0900

    Removed time.js dependancy
    We now call it locally.

--- Main/main.js
+++ Main/main.js
@@ -37,8 +37,15 @@
 				'&nbsp;License</a>' +
 			'</li>' +
 		'</ul>';
+
+	setTime(); //Initial call to show time immediatly
+	setInterval(setTime, 1000); // Set a timer to call function setTime every 1000ms, or 1 second.
 };
 
 function redirectToSite(urlOfSite) {
 	window.open(urlOfSite, "_blank");
+}
+
+function setTime() {
+	document.getElementById("time").innerHTML = new Date().toLocaleTimeString(); // Find the element with ID date, and modify contents to the local time, which gets time from System Clock.
 }
